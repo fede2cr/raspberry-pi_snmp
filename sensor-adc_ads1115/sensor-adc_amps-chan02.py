0262e203303298914251d401502f0d1cd34ec562
Alvaro Figueroa Cabezas <alvaro@greencore.co.cr>
2017-02-01 16:04:34 -0600

    Adding adc_ads1115 with energy monitor shield from CRCibernetica

---
+++ sensor-adc_ads1115/sensor-adc_amps-chan02.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# Copyright (c) 2014 Adafruit Industries
+# Copyright (c) 2017 Greencore Solutions
+# Author: Tony DiCola
+# Modified by: Alvaro Figueroa
+
+# Originally an example for the DHT Python Library by Adafruit
+# Modified to answer as SNMP extension without arguments
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import sys
+
+import Adafruit_ADS1x15
+# Create an ADS1115 ADC (16-bit) instance.
+adc = Adafruit_ADS1x15.ADS1115()
+
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 1
+
+amps_chan02 = adc.read_adc(2, gain=GAIN)
+
+
+if amps_chan02 is not None:
+    print('.1.3.6.1.2.1.25.1.9.3')
+    print('gauge')
+    print('{0:0.1f}'.format(amps_chan02))
+else:
+    print('Failed to get reading. Try again!')
+    sys.exit(1)
